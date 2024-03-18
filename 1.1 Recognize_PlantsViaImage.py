e612fafce1950ae3b146eb33728083847d4955e1
Josh Joy <36846262+joshjomer@users.noreply.github.com>
2024-03-18 18:35:23 +0530

    Update 1.1 Recognize_PlantsViaImage.py

--- 1.1 Recognize_PlantsViaImage.py
+++ 1.1 Recognize_PlantsViaImage.py
@@ -3,7 +3,7 @@ import requests
 import json 
 
 url = 'https://my-api.plantnet.org/v2/identify/all?'  #This the url to which we can upload an image to the plantnet server and get the response of the identified plant 
-api_key = 'api-key=2a10qZqW65kImRBwZrqaEFYhe' #We need to pass our API-Key along with the url which can be found in the plantnet account settings after loging in
+api_key = 'api-key=  ' #We need to pass our API-Key along with the url which can be found in the plantnet account settings after loging in
 imagefile = open('Images/moneyplant.jpg','rb')#We open the image file of the plant we want to identify by passing its location and open in read binary mode.
 data = {'organs':'leaf'} #We specify the part of the plant we want to identify
 
